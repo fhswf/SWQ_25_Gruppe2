af0e82efc4bb1616e82ce574aed40ba7ee636faa
Channaminii <116287562+Channaminii@users.noreply.github.com>
2025-10-11 09:25:12 +0000

    Ü3-A1 - Implementierung der Konto-Klasse

--- Uebung3/Code/konto.py
+++ Uebung3/Code/konto.py
@@ -1,3 +1,5 @@
+# Implementiert von: [HAHR], [PRSE]
+
 """
 Hier sollen Sie Ihre Implementierung der Konto-Klasse erstellen.
 
@@ -8,33 +10,44 @@ from decimal import Decimal
 from .interfaces import KontoInterface
 
 class Konto(KontoInterface):
+
     """
-    TODO: Implementieren Sie diese Klasse
-    
+    Initialisierung der Konto-Klasse
     Regeln:
     - ID wird bei Erstellung mitgegeben (numerisch, darf nicht leer sein)
     - Konto darf nicht überzogen werden (Saldo < 0)
     - Fehler bei inkorrekter Nutzung werfen
     """
-    
     def __init__(self, konto_id: int, saldo: Decimal = Decimal('0.00')):
-        # TODO: Implementierung
-        pass
+        if konto_id is None or not isinstance(konto_id, int) or konto_id <= 0:
+            raise ValueError("Konto-ID muss eine gültige Zahl sein, darf nicht leer sein und größer 0 sein.")
+        if saldo < Decimal('0.00'):
+            raise ValueError("Startsaldo darf nicht negativ sein.")
+        self._konto_id = konto_id
+        self._saldo = saldo
     
+    # Attribut für die Konto-ID
     @property
     def konto_id(self) -> int:
-        # TODO: Implementierung
-        pass
+        return self._konto_id
     
+    # Attribut für den Kontostand
     @property
     def saldo(self) -> Decimal:
-        # TODO: Implementierung
-        pass
+        return self._saldo
     
+    # Methode zum Einzahlen
     def einzahlen(self, betrag: Decimal) -> None:
-        # TODO: Implementierung
-        pass
+        if betrag <= Decimal('0.00'):
+            raise ValueError("Einzahlungsbetrag muss höher als 0 Euro sein.")
+        self._saldo += betrag
     
+    # Methode zum Auszahlen
     def auszahlen(self, betrag: Decimal) -> None:
-        # TODO: Implementierung
-        pass
+        if self._saldo <= Decimal('0.00'):
+            raise ValueError("Konto ist leer, eine Auszahlung ist nicht möglich.")
+        if betrag <= Decimal('0.00'):
+            raise ValueError("Auszahlungsbetrag muss positiv sein.")
+        if self._saldo - betrag < Decimal('0.00'):
+            raise ValueError("Konto darf nicht überzogen werden.")
+        self._saldo -= betrag
